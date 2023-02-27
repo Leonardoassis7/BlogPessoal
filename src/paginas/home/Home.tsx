a950185fb579fcfe7bc2a7b477b23d50c634b84a
Leonardo Assis <leonardorobertoassis@gmail.com>
2023-02-27 13:50:09 -0300

    react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="http://4.bp.blogspot.com/-3YwFT7wiDnE/VT5NLGC20qI/AAAAAAABC8Y/tQcMBoQkbEk/s1600/adffb142a07755f9fc4e1400e3491ae32.jpg" alt="imagem inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
